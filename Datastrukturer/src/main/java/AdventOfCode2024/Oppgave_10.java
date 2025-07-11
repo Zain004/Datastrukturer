ebcb23677cd4f5f96e6ae975f59bb29c3b1cd598
Zain004 <143086801+Zain004@users.noreply.github.com>
2025-07-11 12:41:40 +0200

    laget oppgavetekst og skjønt advent of code oppgave 10

---
+++ Datastrukturer/src/main/java/AdventOfCode2024/Oppgave_10.java
@@ -0,0 +1,42 @@
+package AdventOfCode2024;
+/*
+Et reinsdyr har gitt deg et topografisk kart over Lavaøya,
+der høydene er representert med tall fra 0 (lavest) til 9 (høyest).
+ Kartet består av et rutenett med tall, og du skal finne alle turstier
+ fra toppunkt 0 til 9.
+
+En gyldig tursti:
+    Starter på et tall 0
+    Ender på et tall 9
+
+🧮 Mål: Finn summen av poengsummene til alle stistartpunktene i kartet.
+🧪 Eksempel
+
+Gitt dette kartet:
+
+10..9..
+2...8..
+3...7..
+4567654
+...8..3
+...9..2
+.....01
+for å gå fra 0 til 9 for eksemepl så går ma 0->1->2->3->4->5->6->7->8->9
+
+Her er det to startpunkter (0):
+
+    Startpunktet øverst har poengsum 1
+
+    Startpunktet nederst har poengsum 2
+
+🔢 Totalt: 1 + 2 = 3
+
+Du får input som en liste med strenger (én per rad). Du skal returnere ett tall:
+summen av poengsummene til alle stistartpunktene i kartet.
+ */
+public class Oppgave_10 {
+
+    public static void main(String[] args) {
+        
+    }
+}
