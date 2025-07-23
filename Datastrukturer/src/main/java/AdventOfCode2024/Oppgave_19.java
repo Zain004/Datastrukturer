0e1e66abb6ab9c63ba02b4e1821fb0f7fc6563b4
Zain004 <143086801+Zain004@users.noreply.github.com>
2025-07-23 23:47:47 +0200

    laget oppgave_19 klasse med tekst

---
+++ Datastrukturer/src/main/java/AdventOfCode2024/Oppgave_19.java
@@ -0,0 +1,41 @@
+package AdventOfCode2024;
+/*
+Du er ansatt for å hjelpe en onsen med å organisere håndklærne sine.
+Onsen har en liste over tilgjengelige håndklær med forskjellige stripemønstre,
+ f.eks. "r" (rød), "wr" (hvit-rød) eller "bwu" (blå-hvit-blå). De har også en
+ liste over design de ønsker å lage, f.eks. "brwrr" (blå-rød-hvit-rød-rød).
+ Målet ditt er å finne ut hvor mange av disse designene som kan lages ved å
+ kombinere de tilgjengelige håndklærne.
+
+Eksempel:
+
+Tilgjengelige håndklær: r, wr, b, g, bwu, rb, gb, br
+
+Ønskede design:
+
+brwrr (kan lages med br, wr, r)
+
+bggr (kan lages med b, g, g, r)
+
+gbbr (kan lages med gb, br)
+
+rrbgbr (kan lages med r, rb, g, br)
+
+ubwu (kan ikke lages)
+
+bwurrg (kan lages med bwu, r, r, g)
+
+brgr (kan lages med br, g, r)
+
+bbrgwb (kan ikke lages)
+
+I dette eksemplet kan 6 av 8 design lages. Oppgaven er å skrive kode som leser inn
+tilgjengelige håndklær og ønskede design, og returnerer antall mulige design.
+ */
+public class Oppgave_19 {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
